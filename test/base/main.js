641da1eb8e752f8eca577645682ad8c97d706062
joamag <joamag@hive.pt>
2018-06-13 12:53:13 +0100

    initial travis

---
+++ test/base/main.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe("Simple", function() {
+    describe("#main", function() {
+        it("should run some basic tests", () => {
+            assert.equal(true, true);
+        });
+    });
+});
